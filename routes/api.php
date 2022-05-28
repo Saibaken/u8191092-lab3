4104cc6bda5b62cd8d0ce38bad997c417e25a440
Saibaken <Shelamov.s@gmail.com>
2022-05-28 22:44:21 +0300

    feat: :sparkles: Добавил пути для API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\APIV1\Flower\Controllers\FlowerController;
+use App\Http\APIV1\Room\Controllers\RoomController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/flowers', [FlowerController::class, 'create']);
+Route::delete('/flowers/{id}', [FlowerController::class, 'delete']);
+Route::get('/flowers', [FlowerController::class, 'getAll']);
+Route::get('/flowers/{id}', [FlowerController::class, 'getById']);
+Route::put('/flowers/{id}', [FlowerController::class, 'update']);
+Route::patch('/flowers/{id}', [FlowerController::class, 'updateFields']);
+Route::post('/flowers/{id}/water', [FlowerController::class, 'water']);
+Route::get('/rooms', [RoomController::class, 'getAll']);
